aa4bc7e98ed20534cc075c17e301b759e78c3a50
DavidSanwald <david.sanwald@gmail.com>
2021-03-11 00:36:55 +0100

    use conditional type to model: events with no output handlers with no input

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,44 @@
 type EventMap = Record<string, any>;
-type VoidKeys<T extends EventMap> = {
-  [K in keyof T]: T[K] extends (arg: void) => void ? K : never;
-}[keyof T];
 type NonVoidKeys<T extends EventMap> = {
   [K in keyof T]: T[K] extends (arg: void) => void ? never : K;
 }[keyof T];
 type Unpacked<T> = T extends (infer U)[] ? U : T;
 
 type Handlers<T> = { [K in keyof T]: T[K][] };
-// type EventReceiver<T> = (params: T) => void;
 class Ereignis<T extends EventMap> {
   private handlers: Handlers<T>;
+  private onceHandlers: Handlers<T>;
   constructor() {
     this.handlers = {} as Handlers<T>;
+    this.onceHandlers = {} as Handlers<T>;
   }
-  // emit<K extends VoidKeys<T>>(eventKey: K): void;
-  emit<K extends VoidKeys<T>>(eventKey: K, ...args: never): void;
-  emit<K extends NonVoidKeys<T>>(eventKey: K, ...args: Parameters<T[K]>): void {
-    if (args) {
-      this.handlers[eventKey].forEach(listeningHandler =>
-        listeningHandler(args)
-      );
+
+  emit<K extends keyof T>(
+    eventKey: K,
+    ...fnArgs: K extends NonVoidKeys<T> ? Parameters<T[K]> : never
+  ): void {
+    if (fnArgs && fnArgs.length > 0) {
+      eventKey in this.handlers &&
+        this.handlers[eventKey].length > 0 &&
+        this.handlers[eventKey].forEach(listeningHandler =>
+          listeningHandler(fnArgs)
+        );
+      eventKey in this.onceHandlers &&
+        this.onceHandlers[eventKey].length > 0 &&
+        this.onceHandlers[eventKey].forEach(listeningHandler => {
+          listeningHandler(fnArgs);
+          this.off(eventKey, listeningHandler);
+        });
     } else {
-      this.handlers[eventKey].forEach(listeningHandler => listeningHandler());
+      eventKey in this.handlers &&
+        this.handlers[eventKey].length > 0 &&
+        this.handlers[eventKey].forEach(listeningHandler => listeningHandler());
+      eventKey in this.onceHandlers &&
+        this.onceHandlers[eventKey].length > 0 &&
+        this.onceHandlers[eventKey].forEach(listeningHandler => {
+          listeningHandler();
+          this.off(eventKey, listeningHandler);
+        });
     }
   }
 
@@ -34,9 +50,22 @@ class Ereignis<T extends EventMap> {
       this.handlers[eventKey].push(handler);
     }
   }
+  once<K extends keyof T>(eventKey: K, handler: Unpacked<T[K]>): void {
+    if (!((eventKey as string) in this.onceHandlers)) {
+      this.onceHandlers[eventKey] = [];
+    }
+    if (Array.isArray(this.onceHandlers[eventKey])) {
+      this.onceHandlers[eventKey].push(handler);
+    }
+  }
   off<K extends keyof T>(eventKey: K, handler: Unpacked<T[K]>) {
     if (eventKey in this.handlers) {
       this.handlers[eventKey] = this.handlers[eventKey].filter(
+        (listeningHandler: any) => listeningHandler !== handler
+      );
+    }
+    if (eventKey in this.onceHandlers) {
+      this.onceHandlers[eventKey] = this.onceHandlers[eventKey].filter(
         (listeningHandler: any) => listeningHandler !== handler
       );
     }
